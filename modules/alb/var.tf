ea7e7b67b1c07addb34d6466b880ce915c3c9909
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-16 16:00:42 +0530

    Update var.tf

--- modules/alb/var.tf
+++ modules/alb/var.tf
@@ -1,4 +1,19 @@
-variable "alb_name" {
-  description = "The alb name"
-  default     = "demo-alb"
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs"
+  type        = list(string)
+}
+
+variable "alb_security_group_id" {
+  description = "The security group ID for the ALB"
+  type        = string
+}
+
+variable "instance_id" {
+  description = "The ID of the EC2 instance"
+  type        = string
 }
