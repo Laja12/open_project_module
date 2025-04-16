a073a768e79da0da9af235f7b281f90f5ab88323
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-16 15:22:46 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,81 @@
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+ 
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+  tags = {
+    Name = "demo-igw-gateway"
+  }
+}
+ 
+resource "aws_route_table" "public_rt1" {
+  vpc_id = aws_vpc.demo_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo_igw.id
+  }
+  tags = {
+    Name = "public-route-table1"
+  }
+}
+ 
+resource "aws_route_table_association" "rt_assc_1" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_rt1.id
+}
+ 
+resource "aws_route_table_association" "rt_assc_2" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt1.id
+}
+ 
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = var.public_subnet_a_cidr
+  availability_zone = var.availability_zone_a
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+ 
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = var.public_subnet_b_cidr
+  availability_zone = var.availability_zone_b
+  tags = {
+    Name = "public-subnet-b"
+  }
+}
+ 
+resource "aws_security_group" "demo_sg" {
+  vpc_id = aws_vpc.demo_vpc.id
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  tags = {
+    Name = "demo-sg"
+  }
+}
