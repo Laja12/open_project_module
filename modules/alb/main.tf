bdd1b8d19c23c4fce4300adbdce2d11c26360af4
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-16 15:59:46 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,48 +1,50 @@
-resource "aws_lb" "demo_alb" {
-  name               = var.alb_name
+resource "aws_lb" "openproject_alb" {
+  name               = "openproject-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.demo_sg.id]
-  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
- 
+  security_groups    = [var.alb_security_group_id]
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+
   tags = {
-    Name = "demo-alb"
+    Name = "OpenProject-ALB"
   }
 }
- 
-resource "aws_lb_target_group" "demo_tg" {
-  name     = "demo-tg"
-  port     = 8080
+
+resource "aws_lb_target_group" "openproject_tg" {
+  name     = "openproject-tg"
+  port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.demo_vpc.id
- 
+  vpc_id   = var.vpc_id
+
   health_check {
-    interval            = 30
     path                = "/login"
+    interval            = 30
     timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    matcher             = "200"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
- 
+
   tags = {
-    Name = "demo-tg"
+    Name = "OpenProject-TG"
   }
 }
- 
-resource "aws_lb_listener" "demo_listener" {
-  load_balancer_arn = aws_lb.demo_alb.arn
-  port              = "80"
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.openproject_alb.arn
+  port              = 80
   protocol          = "HTTP"
- 
+
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.demo_tg.arn
+    target_group_arn = aws_lb_target_group.openproject_tg.arn
   }
 }
- 
-resource "aws_lb_target_group_attachment" "demo_tg_attachment" {
-  target_group_arn = aws_lb_target_group.demo_tg.arn
-  target_id        = aws_instance.demo_instance.id
+
+resource "aws_lb_target_group_attachment" "openproject" {
+  target_group_arn = aws_lb_target_group.openproject_tg.arn
+  target_id        = var.instance_id
   port             = 8080
 }
+
