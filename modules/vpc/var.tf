9e6829660bd84757d2a8823279b587e8d817d695
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-16 16:06:02 +0530

    Update var.tf

--- modules/vpc/var.tf
+++ modules/vpc/var.tf
@@ -1,24 +1,11 @@
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
- 
-variable "public_subnet_a_cidr" {
-  description = "The CIDR block for the first public subnet"
-  default     = "10.0.1.0/24"
+
+output "subnet_ids" {
+  value = aws_subnet.public[*].id
 }
- 
-variable "public_subnet_b_cidr" {
-  description = "The CIDR block for the second public subnet"
-  default     = "10.0.2.0/24"
-}
- 
-variable "availability_zone_a" {
-  description = "The availability zone for the first public subnet"
-  default     = "us-east-1a"
-}
- 
-variable "availability_zone_b" {
-  description = "The availability zone for the second public subnet"
-  default     = "us-east-1b"
+
+output "route_table_id" {
+  value = aws_route_table.public.id
 }
